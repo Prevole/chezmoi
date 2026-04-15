4fda9cae5161124d7febd7fa12c22bf672c0a350
Prevole <1104066+Prevole@users.noreply.github.com>
2026-04-15 17:33:25 +0200

    Update .homebrew/Brewfile Update scripts/mac-setup.sh

--- scripts/mac-setup.sh
+++ scripts/mac-setup.sh
@@ -47,37 +47,3 @@ else
   echo "Chezmoi already initialized. Skip."
 fi
 
-#if [ ! -d "$HOME/.oh-my-zsh" ]; then
-#  echo "Installing Oh My Zsh..."
-#  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#
-#  [ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ] || \
-#    git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
-#
-#  [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ] || \
-#    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-#
-#  [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ] || \
-#    git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-#
-#  FONT_URLS=(
-#    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"
-#    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"
-#    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf"
-#    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"
-#  )
-#
-#  for url in "${FONT_URLS[@]}"; do
-#    file_name="$(basename "$url" | sed 's/%20/ /g')"
-#    target="$FONT_DIR/$file_name"
-#
-#    if [ ! -f "$target" ]; then
-#      echo "Downloading font: $file_name"
-#      curl -fL "$url" -o "$target"
-#    else
-#      echo "Font already present: $file_name"
-#    fi
-#  done
-#else
-#  echo "Oh My Zsh already installed. Skip."
-#fi
