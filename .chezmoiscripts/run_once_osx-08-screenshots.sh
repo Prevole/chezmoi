4ed18680e7bc372a88d471eb78e07e0f8a42be59
Prevole <1104066+Prevole@users.noreply.github.com>
2026-04-25 08:37:08 +0200

    Update OSX settings

--- .chezmoiscripts/run_once_osx-08-screenshots.sh
+++ .chezmoiscripts/run_once_osx-08-screenshots.sh
@@ -2,6 +2,13 @@
 
 source "${HOME}/.local/share/chezmoi/scripts/macos-flags.zsh"
 
+# Screenshots - save location
+mkdir -p "${HOME}/Downloads/screenshots"
+defaults write com.apple.screencapture location -string "${HOME}/Downloads/screenshots"  # ok
+
+# Screenshots - format
+defaults write com.apple.screencapture type -string "png"  # ok
+
 # Screenshots - disable window shadow
 defaults write com.apple.screencapture disable-shadow -bool true  # ok
 
